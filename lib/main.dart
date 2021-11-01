8f3a8de833d1afc57a6e1fe7a2b1b64e030b5c6b
Misti <mistier.famous@gmail.com>
2021-11-01 21:45:58 +0600

    123

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,32 @@
-import 'package:visitapp/main.dart';
-
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'generated/l10n.dart';
 
 void main(){
+  runApp(MyApp());
+}
 
+
+
+
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      localizationsDelegates: [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      title: 'Flutter Demo',
+      home: Scaffold(
+        body: Center(child: Text("I18"),),
+      ),
+    );
+  }
 }
