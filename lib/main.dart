3be73905dce08a9127074961459de4a472c4d209
idlkz <idlkz2019@gmail.com>
2021-11-01 21:50:43 +0600

    111

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,28 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+
 import 'generated/l10n.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
-
-
-
-
-
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      localizationsDelegates: [
+      localizationsDelegates: const [
         S.delegate,
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
       supportedLocales: S.delegate.supportedLocales,
-      title: 'Flutter Demo',
-      home: Scaffold(
-        body: Center(child: Text("I18"),),
-      ),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(),
+      home: Scaffold(),
     );
   }
 }
+
